31283f6884ae84243004ae8ab74371d46b216ea0
Alexey <qoqo.s@yandex.ru>
2022-02-16 15:01:22 +0300

    initial commit

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
